4bcaeee6d4d808971543fd49de757de4999d68a0
haley <haley@MAINPC>
2022-07-20 19:41:51 -0700

    agility

--- RSAutoCLicker/skilling/Agility.cs
+++ RSAutoCLicker/skilling/Agility.cs
@@ -14,7 +14,38 @@
         public void Do()
         {
             Console.WriteLine($"Doing {nameof(Agility)}");
-            // todo
+
+            // click tree
+            _mouseHandler.SmoothLeftClick(1277, 171, true, true);
+            Thread.Sleep(2300);
+
+            // click wall
+            _mouseHandler.SmoothLeftClick(810, 561, true, true);
+            Thread.Sleep(4300);
+
+            // click gap
+            _mouseHandler.SmoothLeftClick(566, 397, true, true);
+            Thread.Sleep(4850);
+
+            // click tightrope
+            _mouseHandler.SmoothLeftClick(810, 761, true, true);
+            Thread.Sleep(8000);
+
+            // click gap
+            _mouseHandler.SmoothLeftClick(938, 711, true, true);
+            Thread.Sleep(3200);
+
+            // click gap
+            _mouseHandler.SmoothLeftClick(646, 668, true, true);
+            Thread.Sleep(4000);
+
+            // click edge
+            _mouseHandler.SmoothLeftClick(986, 535, true, true);
+            Thread.Sleep(2000);
+
+            // teleport reset
+            _mouseHandler.SmoothLeftClick(1526, 754, true, true);
+            Thread.Sleep(2000);
         }
     }
 }
